64a9cc60be0c96adb9d74669e4e4abc8d99119b9
Rilquer <38020251+Rilquer@users.noreply.github.com>
2024-07-11 18:04:47 -0400

    Plots and hill numbers

--- evo-bio/bird-barcode/aln_gendiv.R
+++ evo-bio/bird-barcode/aln_gendiv.R
@@ -47,36 +47,61 @@ coi_data <- coi_data %>% mutate(pi = nuc.div(as.DNAbin(aln[[1]])))
 ## Plotting abundance and genetics
 
 comm <- readRDS(file = 'data/bird/abundance/comm_counts_samples.rds')
-comm <- lapply(1:length(comm),function(x){return(mutate(comm[[x]],island = rep(names(comm)[x],nrow(comm[[x]]))))}) %>%
-  do.call(what = rbind.data.frame) %>% mutate(island = str_to_lower(island)) %>%
-  rename(sciName = 'binomial') %>% 
-  mutate(binomial = fct_reorder(binomial,dplyr::desc(total)))
+comm_gen <- vector('list')
+for (x in names(comm)) {
+  comm_gen[[x]] <- comm[[x]] %>% 
+    rename(sciName = 'binomial') %>%
+    left_join(coi_data[which(coi_data$island==str_to_lower(x)),],by='binomial') %>% 
+    select(!c(sequence,aln,island)) %>%
+    mutate(binomial = fct_reorder(binomial,dplyr::desc(total)))
+}
+names(comm_gen) <- names(comm)
 
-comm %>%
-  ggplot(aes(x=fct_reorder(binomial,dplyr::desc(total)),y=total,group=island))+
+coeff <- 10^4 # Coefficient for second axis
+
+i=1
+ggplot(comm_gen[[i]],aes(x=binomial,y=total))+
   geom_bar(stat='identity',position = position_dodge(),fill='cadetblue3')+
-  #stat_smooth(aes(y=total, x=binomial), method = lm, formula = y ~ poly(x, 10), se = FALSE,
-  #            col = 'red',size=0.5)+
-  facet_wrap(~island)
-
-  left_join(coi_data,by=c('island','binomial')) %>%
-  select(!c(sequence,aln,)) %>%
-  
-coeff <- 10^3.2 # Coefficient for second axis
-
-library(ggthemes)
-comm %>%
-  ggplot(aes(x=binomial,y=total,group=island))+
-  geom_bar(stat='identity',position = position_dodge(),fill='cadetblue3')+
-  stat_smooth(aes(y=total, x=binomial), method = lm, formula = y ~ poly(x, 10), se = FALSE,
-              col = 'red',size=0.5)+
-  geom_point(aes(y=pi*coeff),color='purple',size=1.5)+
-  facet_wrap(~island)+
+  stat_smooth(aes(y=total, x=as.numeric(binomial)), method = lm,, formula = y ~ poly(x, 10),se = FALSE,
+              col = 'red',size=0.4,linetype='dashed')+
+  geom_point(aes(y=pi*coeff),color='purple',size=3.5)+
   scale_y_continuous(
     name = "Abundance",
-    sec.axis = sec_axis(~./coeff, name="Nuc. Div.")
+    sec.axis = sec_axis(~./coeff)
   )+
   labs(x='Species')+
   theme_minimal()+
+  #theme_light()+
   theme(axis.text.x = element_blank(),
-        axis.ticks = element_blank())
+        axis.ticks = element_blank(),
+        axis.text.y = element_text(size=17),
+        axis.title.x = element_blank(),
+        #axis.title.x = element_text(size=18, vjust = 0.1),
+        axis.title.y = element_text(size=20, vjust = 1.8),
+        plot.title = element_text(size=26))+
+  ggtitle(names(comm_gen)[i])
+ggsave(paste0('output/',names(comm_gen)[i],'_SAD_pi.png'),width = 9,height = 7,dpi=1200)
+
+## Hill numbers
+
+x <- lapply(comm,select,c(1,2))
+# Getting spp-site matrix
+getMat <- function (x) {
+  sppmat <- x[[1]]
+  colnames(sppmat)[2] <- names(x)[1]
+  if (length(x)>1) {
+    for (i in 2:length(x)) {
+      colnames(x[[i]])[2] <- names(x)[i]
+      sppmat <- sppmat %>% full_join(x[[i]])
+    }
+  }
+  sppmat <- sppmat %>% mutate(across(2:ncol(sppmat),~replace_na(.x,0)))
+  allspp <- sppmat$sciName
+  sppmat <- data.frame(sppmat[,2:ncol(sppmat)])
+  rownames(sppmat) <- allspp
+  return(t(sppmat))
+}
+sppmat <- getMat(x)
+require(hillR)
+taxahill <- sapply(0:3,hill_taxa, comm = sppmat)
+colnames(taxahill) <- paste0('abund_h',0:3)
